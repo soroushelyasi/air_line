531ca0ea8f6b968220f43ccb2c28cabfe85e6a9c
soroushelyasi <soroush.elyasi343@gmail.com>
2024-12-20 17:09:36 +0330

    add Flight

---
+++ flight.py
@@ -0,0 +1,9 @@
+class Flight:
+    def __init__(self, flight_number,airplane,city_from,city_to,number_of_stops,list_of_city_for_stop_in_order):
+        self.flight_number =flight_number
+        self.airplane =airplane
+        self.city_from=city_from
+        self.city_to =city_to
+        self.number_of_stops =number_of_stops
+        self.list_of_city_for_stop_in_order =list_of_city_for_stop_in_order
+        
