a15c12330893a2dfa428ae5e0c061133690b7c73
soroushelyasi <soroush.elyasi343@gmail.com>
2024-12-20 17:02:07 +0330

    first init

---
+++ airplane.py
@@ -0,0 +1,5 @@
+class Airplane:
+    def __init__(self, name, airplane_class, number_of_passengers):
+        self.name = name
+        self.airplane_class = airplane_class
+        self.number_of_passengers = number_of_passengers
---
+++ city.py
@@ -0,0 +1,5 @@
+class City:
+    def __init__(self, name, longitude, latitude):
+        self.name = name
+        self.longitude = longitude    # x
+        self.latitude = latitude      # y
