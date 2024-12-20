5d9db799a29c836e5fda1bc91b378c499f112763
soroushelyasi <soroush.elyasi343@gmail.com>
2024-12-20 17:23:45 +0330

    add passanger to flieght

--- flight.py
+++ flight.py
@@ -1,8 +1,9 @@
 class Flight:
-    def __init__(self, flight_number, airplane, city_from, city_to, number_of_stops, list_of_city_for_stop_in_order):
+    def __init__(self, flight_number, airplane, city_from, city_to, number_of_stops, list_of_city_for_stop_in_order, set_of_passenger):
         self.flight_number = flight_number
         self.airplane = airplane
         self.city_from = city_from
         self.city_to = city_to
         self.number_of_stops = number_of_stops
         self.list_of_city_for_stop_in_order = list_of_city_for_stop_in_order
+        self.set_of_passenger=set_of_passenger
