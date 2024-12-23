26bd5c0e74f90b884a8ee585da013eb6cb355822
soroushelyasi <soroush.elyasi343@gmail.com>
2024-12-23 11:11:28 +0330

    add storage.py

---
+++ storage.py
@@ -0,0 +1,7 @@
+class Storage:
+    def __init__(self, air_plans,cities,flights, passengers):
+        self.air_plans =air_plans
+        self.cities =cities
+        self.flights =flights
+        self.passengers =passengers
+        
